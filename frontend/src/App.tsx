3bc9503a1189f66eb8200ebb3e36ddab07d2e3a4
ricardozsp1515-stack <ricardozsp1515@gmail.com>
2026-06-06 13:01:28 -0600

    Daisy UI ya funcional

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,20 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
-
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Daisy_test from "./components/Daisy_test"
 
 function App() {
 
   return (
     <>
-      
+
+      <Router>
+        <Routes>
+          <Route path="/test" element={<Daisy_test></Daisy_test>}></Route>
+        </Routes>
+      </Router>
+
     </>
   )
 }
